1220063fec351287d2c0a15755b907c3c0fc2c7c
SultanTipa1 <josephngumbaugush@gmail.com>
2024-10-12 17:01:37 +0300

    Adds all files

--- server/seed.py
+++ server/seed.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python3
 #server/seed.py
+from random import choice as rc
+from faker import Faker
 
 from app import app
 from models import db, Pet
 
 with app.app_context():
 
+    # Create and initialize a faker generator
+    fake = Faker()
+
+    # Delete all rows in the "pets" table
+    Pet.query.delete()
+
     # Create an empty list
     pets = []
 
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+
     # Add some Pet instances to the list
+    for n in range(10):
+        pet = Pet(name=fake.first_name(), species=rc(species))
+        pets.append(pet)
     pets.append(Pet(name = "Fido", species = "Dog"))
     pets.append(Pet(name = "Whiskers", species = "Cat"))
     pets.append(Pet(name = "Hermie", species = "Hamster"))
+    pets.append(Pet(name = "Slither", species = "Snake"))
+    pets.append(Pet(name = "Bob", species = "Turtle"))
+    pets.append(Pet(name = "Clerk", species = "Hamster"))
+    pets.append(Pet(name = "Croo", species = "Chicken"))
+    pets.append(Pet(name = "Lex", species = "Dog"))
+    pets.append(Pet(name = "Bob", species = "Cat"))
+    pets.append(Pet(name = "Siri", species = "Chicken"))
 
     # Insert each Pet in the list into the database table
     db.session.add_all(pets)
